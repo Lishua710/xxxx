6f6a917c95a907b5be23fff995a58b718fbde3e1
Lishua710 <119596706+Lishua710@users.noreply.github.com>
2024-04-10 00:02:03 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'kuangjia.tk'
+      url.hostname = 'bt.k439.link'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
